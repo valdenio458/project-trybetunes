9d4e1885c1ac232729480707e22fde827cf3dacd
valdenio458 <valdeniomartinho@gmail.com>
2021-10-19 17:46:00 -0300

    adicona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NoteFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+
+        <Switch>
+          
+          <Route exact path="/profile/edit">
+            <ProfileEdit />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites />
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+          <Route exact path="/album/:id">
+            <Album />
+          </Route>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="*/">
+            <NoteFound />
+          </Route>
+        </Switch>
+
+      </BrowserRouter>
+    );
   }
 }
 
