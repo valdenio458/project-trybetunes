30dfe75eb2ae6ede580de5aff0a8d956a558cb8f
valdenio458 <valdeniomartinho@gmail.com>
2021-10-21 07:43:55 -0300

    adiciona requisito 3

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../componentes/Header';
 
 class Profile extends React.Component {
   render() {
     return (
       <div data-testid="page-profile">
         profile
+        <Header />
       </div>
     );
   }
--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../componentes/Header';
 
 class ProfileEdit extends React.Component {
   render() {
     return (
       <div data-testid="page-profile-edit">
         ProfileEdit
+        <Header />
       </div>
     );
   }
