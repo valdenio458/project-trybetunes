59b261d63f9a6d3e343d786fbeffe5715e84f361
valdenio458 <valdeniomartinho@gmail.com>
2021-10-21 17:12:58 -0300

    adiciona requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../componentes/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -8,16 +11,52 @@ class Search extends React.Component {
     this.state = {
       artist: '',
       isButtonDisabled: true,
+      isLoading: false,
+      result:  '', 
+      albums: [],
 
     };
     this.handleInput = this.handleInput.bind(this);
     this.validateForm = this.validateForm.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { artist } = this.state;
+    this.setState({
+      isLoading: true,
+      artist,
+    }, async () => {
+      const albumsFound = await searchAlbumsAPI(artist);
+      this.setState({
+        isLoading: false,
+        albums: albumsFound ? [...albumsFound] : [],
+        artist: '',
+      });
+    });
   }
 
   handleInput({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value }, () => this.validateForm());
+    this.setState({ [name]: value, result: value }, () => this.validateForm());
+  }
+
+  getAlbums() {
+    const { albums } = this.state;
+    return (albums.map((album) => (
+      <div key={ album.collectionId }>
+        <Link
+          to={ `/album/${album.collectionId}` }
+          data-testid={ `link-to-album-${album.collectionId}` }
+        >
+          <p>{ album.artistName }</p>
+          <p>{ album.collectionName }</p>
+          <p>{ album.trackCount }</p>
+          <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+        </Link>
+      </div>
+    )));
   }
 
   validateForm() {
@@ -31,28 +70,42 @@ class Search extends React.Component {
   }
 
   render() {
-    const { artist, isButtonDisabled } = this.state;
+    const { artist, isButtonDisabled, isLoading, albums, result} = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-        <form>
-          <input
-            name="artist"
-            data-testid="search-artist-input"
-            value={ artist }
-            onChange={ this.handleInput }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ isButtonDisabled }
+      isLoading ? <Loading />
+        : (
+          <div data-testid="page-search">
+            <Header />
+            <form>
+              <input
+                name="artist"
+                data-testid="search-artist-input"
+                value={ artist }
+                onChange={ this.handleInput }
+              />
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ isButtonDisabled }
+                onClick={ this.handleClick }
 
-          >
-            Pesquisar
+              >
+                Pesquisar
 
-          </button>
-        </form>
-      </div>
+              </button>
+            </form>
+
+            <section>
+              <p>
+                Resultado de álbuns de:
+                { ' ' }
+                { result }
+              </p>
+              { albums.length > 0 ? this.getAlbums() : <p>Nenhum álbum foi encontrado</p>}
+            </section>
+
+          </div>)
+
     );
   }
 }
